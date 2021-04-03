bb2cced0b0589b6c66a2f1fbb5ea084ce04bd7e5
Ari Koskenvirta <53196259+arikoskenvirta@users.noreply.github.com>
2021-04-03 23:28:41 +0300

    RecyclerView with ViewBinding
    voisi tehdä muihinkin näyttöihin

--- app/src/main/java/com/example/recyclerlist/MainActivity.java
+++ app/src/main/java/com/example/recyclerlist/MainActivity.java
@@ -17,6 +17,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.recyclerlist.databinding.ActivityMainBinding;
+import com.example.recyclerlist.databinding.OneLinePersonBinding;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -30,12 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main";
 
     private FirebaseFirestore firebaseFirestore;
-    private RecyclerView mFirestoreRecyclerView;
     private FirestoreRecyclerAdapter adapter;
-
-    private ImageView iw_profilePicture;
-
-    //Button btn_addNew;
+    //private ImageView iw_profilePicture;
 
 
     @Override
@@ -53,22 +51,22 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+        setContentView(view);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Query query = db.collection("persons");
 
-        mFirestoreRecyclerView = findViewById(R.id.rl_recyclerList);
+        RecyclerView mFirestoreRecyclerView;
+        mFirestoreRecyclerView = activityMainBinding.rlRecyclerList;
 
-        //rl_recyclerList options
+        //rl_recyclerList options eli haetaan kannasta listan data
         FirestoreRecyclerOptions<Person> options = new FirestoreRecyclerOptions.Builder<Person>()
                 .setQuery(query, Person.class)
                 .build();
@@ -78,8 +76,9 @@ public class MainActivity extends AppCompatActivity {
             @NonNull
             @Override
             public PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_line_person, parent, false);
-                return new PersonViewHolder(view);
+                LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+                OneLinePersonBinding oneLinePersonBinding = OneLinePersonBinding.inflate(layoutInflater, parent, false);
+                return new PersonViewHolder(oneLinePersonBinding);
             }
 
             @Override
@@ -93,58 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 )
                         .into(holder.iw_profilePicture);
 
-                //                //        "https://firebasestorage.googleapis.com/v0/b/recyclerlist-f7b76.appspot.com/o/ProfilePictures%2FSARI?alt=media&token=49f7cc71-a626-4cab-84d6-abc869e1ad58"
-                /*
-                storageReference = FirebaseStorage.getInstance().getReference().child("ProfilePicture/" + model.getName() );
-
-                try {
-                    final File localFile = File.createTempFile("test", ".jpg");
-
-                    storageReference.getFile(localFile )
-                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-
-                            Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
-                            firebaseUri.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-
-                                    String url = uri.toString();
-                                    Glide.with(MainActivity.this).load(url).into(holder.iw_profilePicture);
-                                    //
-                                    //update to profile url field
-
-                                }
-                            });
-
-
-
-                            //Bitmap bitmap = BitmapFactory.decodeFile(model.getProfilePicture());
-                            //((ImageView) findViewById(R.id.iw_profilePicture)).setImageBitmap(bitmap);
-                            //((de.hdodenhof.circleimageview.CircleImageView) findViewById(R.id.iw_personPicture)).setImageBitmap(bitmap);
-
-
-                            //holder.iw_personPicture2.setImageResource();
-
-
-                            Glide.with(MainActivity.this).load(model.getProfilePicture()).into(holder.iw_profilePicture);
-                            //Glide.with(MainActivity.this).load(model.getProfilePicture()).into(findViewById(R.id.iw_profilePicture)));
-                            //Glide.with(MainActivity.this).load(localFile.getAbsoluteFile())
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-                 */
 
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -170,25 +117,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    private class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private class PersonViewHolder extends RecyclerView.ViewHolder {
         private TextView tv_name;
         private TextView tv_age;
         private ImageView iw_profilePicture;
         private de.hdodenhof.circleimageview.CircleImageView iw_personPicture2;
 
-        public PersonViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tv_name = itemView.findViewById(R.id.tv_name);
-            tv_age = itemView.findViewById(R.id.tv_age);
-            iw_profilePicture = itemView.findViewById(R.id.iw_profilePicture);
-        }
+        OneLinePersonBinding oneLinePersonBinding;
 
-        @Override
-        public void onClick(View v) {
-            //Intent intent = new Intent(MainActivity.this, AddOrEditOne.class);
-            //startActivity(intent);
+        public PersonViewHolder(@NonNull OneLinePersonBinding oneLinePersonBinding) {
+            super(oneLinePersonBinding.getRoot());
+            this.oneLinePersonBinding = oneLinePersonBinding;
 
+            tv_name = oneLinePersonBinding.tvName;
+            tv_age = oneLinePersonBinding.tvAge;
+            iw_profilePicture = oneLinePersonBinding.iwProfilePicture;
+
+            this.oneLinePersonBinding.imageElasticView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //StorageReference storageReference;
+
+                    DocumentSnapshot snapshot;
+                    snapshot = (DocumentSnapshot) adapter.getSnapshots().getSnapshot(getAdapterPosition());
+                    String uid = snapshot.getId();
+
+                    Intent intent = new Intent(MainActivity.this, AddOrEditPerson.class);
+                    intent.putExtra("uid", uid);
+
+                    MainActivity.this.startActivity(intent);
+                }
+            }
+            );
         }
     }
 
@@ -203,7 +163,5 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         adapter.startListening();
     }
-
-
 
 }
